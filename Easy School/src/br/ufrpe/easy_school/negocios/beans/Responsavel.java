4295d04639457690bfa98f49d1d0f85048335619
Paulo Henrique <paulohn27@hotmail.com>
2018-10-26 11:14:04 -0300

    modificacoes

---
+++ Easy School/src/br/ufrpe/easy_school/negocios/beans/Responsavel.java
@@ -0,0 +1,15 @@
+package br.ufrpe.easy_school.negocios.beans;
+
+import java.util.ArrayList;
+
+public class Responsavel {
+	
+	private String nome;
+	private String cpf;
+	private String senha;
+	
+	private ArrayList<Aluno> filhos;
+	private ArrayList<String> reclamacoes;
+	private ArrayList<String> notificacoes;
+
+}
