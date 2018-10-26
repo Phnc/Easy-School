f816cfa5bbeaae8930ad9b7061407f32979b9445
rahmonjorge <44070986+rahmonjorge@users.noreply.github.com>
2018-10-26 20:36:18 -0300

    Estrutura básica adicionada

--- Easy School/src/br/ufrpe/easy_school/negocios/beans/Diretoria.java
+++ Easy School/src/br/ufrpe/easy_school/negocios/beans/Diretoria.java
@@ -1,9 +1,24 @@
 package br.ufrpe.easy_school.negocios.beans;
 
 public class Diretoria {
-	//A classe diretoria tem acesso aos repositorios de alunos, professores e responsáveis e pode modificar informações dentro delas
-	//por exemplo, a diretoria pode "remover" um professor e depois "adicionar" um novo. Basicamente, ela é responsável pelo CRUD das outras classes
-	//Além de poder visualizar informações sobre elas
-	//Imagine que um aluno sofre um acidente na escola, a diretoria tem acesso à informações relevantes neste caso, como tipo sanguineo e alergias.
-
+	//A classe diretoria tem acesso aos repositorios de alunos, professores e responsÃ¡veis e pode modificar informaÃ§Ãµes dentro delas
+	//por exemplo, a diretoria pode "remover" um professor e depois "adicionar" um novo. Basicamente, ela Ã© responsÃ¡vel pelo CRUD das outras classes
+	//AlÃ©m de poder visualizar informaÃ§Ãµes sobre elas
+	//Imagine que um aluno sofre um acidente na escola, a diretoria tem acesso Ã  informaÃ§Ãµes relevantes neste caso, como tipo sanguineo e alergias.
+	
+	public static void adicionarAluno(String nome, String cpf, String tipoSanguineo, String senha, String matricula, String nomeResponsavel) {
+		
+	}
+	
+	public static void removerAluno(String cpf, String matricula) {
+		
+	}
+	
+	public static void adicionarProfessor(String nome, String cpf, String senha) {
+		
+	}
+	
+	public static void removerProfessor(String cpf) {
+		
+	}
 }
