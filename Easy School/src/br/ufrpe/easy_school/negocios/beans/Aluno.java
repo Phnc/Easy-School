172c0e0be9032844aa2b161c8fb63e76e9240adc
RavenbornIV <41174251+RavenbornIV@users.noreply.github.com>
2018-10-27 15:26:53 -0300

    Update Aluno.java

--- Easy School/src/br/ufrpe/easy_school/negocios/beans/Aluno.java
+++ Easy School/src/br/ufrpe/easy_school/negocios/beans/Aluno.java
@@ -52,7 +52,14 @@ public class Aluno {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
-
+	
+	public int getIdade(){
+		return idade;
+	}
+	
+	public void setIdade(int idade){
+		this.idade = idade;
+	}
 
 	public String getCpf() {
 		return cpf;
