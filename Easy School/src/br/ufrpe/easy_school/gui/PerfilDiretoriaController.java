7667d36014b459527d55cca23a66ae458b9884d0
Paulo Henrique <paulohn27@hotmail.com>
2018-12-25 14:44:17 -0300

    Cadastros da diretoria funcionando

--- Easy School/src/br/ufrpe/easy_school/gui/PerfilDiretoriaController.java
+++ Easy School/src/br/ufrpe/easy_school/gui/PerfilDiretoriaController.java
@@ -7,6 +7,7 @@ import br.ufrpe.easy_school.negocios.beans.Professor;
 import br.ufrpe.easy_school.negocios.beans.Responsavel;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.ChoiceBox;
@@ -15,6 +16,8 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.ToggleGroup;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 public class PerfilDiretoriaController {
@@ -204,6 +207,68 @@ public class PerfilDiretoriaController {
     	ObservableList<Disciplina> disciplinas = FXCollections.observableArrayList();
     	disciplinas.addAll(EscolaFachada.getInstance().getDisciplinas());
     	return disciplinas;
+    }
+    
+    public void cadastrar(ActionEvent event) {
+    	if(!textNomeCadastro.getText().isEmpty() && textNomeCadastro.getText().length() > 0 && !textNomeCadastro.getText().equals("") && (!textIdCadastro.getText().isEmpty() && textIdCadastro.getText().length() > 0 && !textIdCadastro.getText().equals("")) && (!textSenhaCadastro.getText().isEmpty() && textSenhaCadastro.getText().length() > 0 && !textSenhaCadastro.getText().equals(""))) {
+    		
+    				if(btnSelectAluno.isSelected()) {
+    					EscolaFachada.getInstance().cadastrarPessoa(new Aluno(textNomeCadastro.getText(), textIdCadastro.getText(), textSenhaCadastro.getText()));
+    					tblAlunos.refresh();
+    					textSenhaCadastro.setText(null);
+    					textNomeCadastro.setText(null);
+    					textIdCadastro.setText(null);
+    					
+    					Alert a = new Alert(AlertType.INFORMATION);
+    					a.setTitle("Informação do cadastro");
+    					a.setHeaderText("Cadastro realizado com sucesso");
+    					a.setContentText(null);
+    					a.show();
+    				}
+    				else if(btnSelectProf.isSelected()) {
+    					EscolaFachada.getInstance().cadastrarPessoa(new Professor(textNomeCadastro.getText(), textIdCadastro.getText(), textSenhaCadastro.getText()));
+    					tblProfessores.refresh();
+
+    					textSenhaCadastro.setText(null);
+    					textNomeCadastro.setText(null);
+    					textIdCadastro.setText(null);
+    					
+    					Alert a = new Alert(AlertType.INFORMATION);
+    					a.setTitle("Informação do cadastro");
+    					a.setHeaderText("Cadastro realizado com sucesso");
+    					a.setContentText(null);
+    					a.show();
+    				}
+    				else if(btnSelectResp.isSelected()) {
+    					EscolaFachada.getInstance().cadastrarPessoa(new Responsavel(textNomeCadastro.getText(), textIdCadastro.getText(), textSenhaCadastro.getText()));
+    					tblResponsaveis.refresh();
+
+    					textSenhaCadastro.setText(null);
+    					textNomeCadastro.setText(null);
+    					textIdCadastro.setText(null);
+    					
+
+    					Alert a = new Alert(AlertType.INFORMATION);
+    					a.setTitle("Informação do cadastro");
+    					a.setHeaderText("Cadastro realizado com sucesso");
+    					a.setContentText(null);
+    					a.show();
+    				}
+    			
+    		
+    	}
+    	
+    	else {
+    		Alert a = new Alert(AlertType.ERROR);
+    		a.setTitle("Cadastro não realizado");
+    		a.setHeaderText("O cadastro não foi realizado com sucesso");
+    		a.setContentText("Verifique se todos os campos estão preenchidos e tente novamente");
+    		a.show();
+    	}
+    	
+    	
+    	initialize();
+    	
     }
 
 }
