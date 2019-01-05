aaeb671f82e6a1d09d677b5353a1d015c7e70a21
MarcusMiguel <44274460+MarcusMiguel@users.noreply.github.com>
2019-01-05 15:28:25 -0200

    Classe para enviar msg através da diretoria

---
+++ Easy School/src/br/ufrpe/easy_school/gui/EnviarMensagemDiretoriaController.java
@@ -0,0 +1,83 @@
+package br.ufrpe.easy_school.gui;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.Alert.AlertType;
+import br.ufrpe.easy_school.negocios.EscolaFachada;
+import br.ufrpe.easy_school.negocios.KeepPerson;
+import br.ufrpe.easy_school.negocios.Mensagem;
+import br.ufrpe.easy_school.negocios.beans.Diretoria;
+import br.ufrpe.easy_school.negocios.beans.Pessoa;
+import br.ufrpe.easy_school.negocios.beans.Responsavel;
+import javafx.event.ActionEvent;
+
+public class EnviarMensagemDiretoriaController {
+	
+	private Pessoa receptor;
+	
+    @FXML
+    private TextArea textMensagem;
+
+    @FXML
+    private Label lblMsg1;
+
+    @FXML
+    private TextField txtTitulo;
+
+    @FXML
+    private Label lblMsg;
+
+    @FXML
+    private Label lblMsg2;
+
+    @FXML
+    private Button btnBack;
+
+    @FXML
+    private Button btnSend;
+
+    @FXML
+    void goBack(ActionEvent event) {
+     	ScreenManager.getInstance().showPerfilDiretoria();
+    }
+    
+    @FXML
+    public void iniciar_Receptor(Pessoa p) {
+    	this.receptor = p;
+      	lblMsg2.setText(receptor.getName());
+    }
+    
+    @FXML
+    void sendMessage(ActionEvent event) {
+    	Mensagem m;
+    	Diretoria d =((Diretoria)EscolaFachada.getInstance().buscar("admin"));
+    	Responsavel r = (Responsavel) receptor;
+    	if(txtTitulo.getText() != null && !txtTitulo.getText().equals("") && txtTitulo.getText().length() > 0) {
+    		if(textMensagem.getText() != null && !textMensagem.getText().equals("") && textMensagem.getText().length() > 0) {
+    			m = new Mensagem(txtTitulo.getText(), textMensagem.getText(), " ", d);
+    			d.addMensagem(m);
+    			r.addMensagem(m);
+    			if(d.getMensagens().contains(m) && r.getMensagens().contains(m)) {
+    	    		Alert a = new Alert(AlertType.INFORMATION);
+    	        	a.setTitle("Confirmação da mensagem");
+    	        	a.setHeaderText("");
+    	        	a.setContentText("Mensagem enviada com sucesso!");
+    	        	a.show();
+    	        	ScreenManager.getInstance().showPerfilDiretoria();
+    	    	}
+    		}
+    	}
+    	else {
+    		Alert a = new Alert(AlertType.WARNING);
+    		a.setTitle("Campos vazios");
+    		a.setContentText("Todos os campos são obrigatórios!");
+    		a.setHeaderText("");
+    		a.show();
+    	}
+    }
+
+}
